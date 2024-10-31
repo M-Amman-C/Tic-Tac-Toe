f1fd6da86e69fd31ad79d992ee59240645808fb2
MohammedAmman Chopadiya <109091158+M-Amman-C@users.noreply.github.com>
2024-10-31 19:30:09 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/ec2-user/Tic-Tac-Toe
+cd /home/ubuntu/Tic-Tac-Toe
 npm install
 pm2 start index.js --name "node-app"  # Replace "app.js" with your entry file name
 pm2 save  # Save the pm2 process list to start on reboot
